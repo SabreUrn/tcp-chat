9711a5905174f75292ebf5b6a548faddbde9731b
Kasp5953 <kasp5953@edu.easj.dk>
2018-02-07 11:07:42 +0100

    adds client connection retrying via try-catch

--- TcpChatClient/TcpChatClient/Program.cs
+++ TcpChatClient/TcpChatClient/Program.cs
@@ -8,8 +8,18 @@ using System.Threading.Tasks;
 namespace TcpChatClient {
     class Program {
         static void Main(string[] args) {
-            TcpClient clientSocket = new TcpClient("localhost", 6789);
-
+            bool serverFound = false;
+            while (!serverFound) {
+                try {
+                    TcpClient clientSocket = new TcpClient("localhost", 6789);
+                    serverFound = true;
+                } catch (SocketException) {
+                    Console.WriteLine("Cannot find server. Check if server is running.");
+                    Console.WriteLine("Retrying in 5 seconds.");
+                    System.Threading.Thread.Sleep(5000);
+                }
+            }
+            Console.WriteLine("Successfully connected to server.");
             Console.ReadLine();
         }
     }
