37efefaa778122acd6ebdbcf676fd26539f3bccd
Kasp5953 <kasp5953@edu.easj.dk>
2018-02-07 10:42:45 +0100

    adds basic TcpListener functionality

--- TcpChatServer/TcpChatServer/Program.cs
+++ TcpChatServer/TcpChatServer/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TcpChatServer {
     class Program {
         static void Main(string[] args) {
+            TcpListener serverSocket = new TcpListener(6789);
+            serverSocket.Start();
+            Console.WriteLine("Server started.");
+
+            TcpClient connectionSocket = serverSocket.AcceptTcpClient();
+            Console.WriteLine("Connection accepted, server activated.");
+
+            Console.ReadLine();
         }
     }
 }
