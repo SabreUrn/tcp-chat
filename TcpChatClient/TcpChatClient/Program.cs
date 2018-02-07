f0cb1be11004ee3e5837aab2b51380df690f4906
Kasp5953 <kasp5953@edu.easj.dk>
2018-02-07 10:53:39 +0100

    adds basic TcpClient functionality

--- TcpChatClient/TcpChatClient/Program.cs
+++ TcpChatClient/TcpChatClient/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TcpChatClient {
     class Program {
         static void Main(string[] args) {
+            TcpClient clientSocket = new TcpClient("localhost", 6789);
+
+            Console.ReadLine();
         }
     }
 }
